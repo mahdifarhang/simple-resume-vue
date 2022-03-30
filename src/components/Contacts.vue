824863f7aea4b72ec0ac3eedb34bec7aeadfc4e4
Mahdi <mafarhang76@gmail.com>
2022-03-30 16:12:35 +0430

    contacts section be more beautiful and accurate

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -1,24 +1,24 @@
 <template>
   <Title :text="contactsLabel" />
   <div v-if="phoneNumber" class="contact-line">
-    <img src="@/assets/phone.svg" />
-    <a class="nav-link nav-social" style="text-decoration: none;" :href="'tel:+' + phoneNumber" title="Phone Number">
-      <Paragraph class="contact" :text="'+' + phoneNumber" />
+    <a class="nav-link nav-social" style="text-decoration: none;" :href="'tel:' + phoneNumber" title="Telephone">
+      <img src="@/assets/phone.svg" />
     </a>
+    <Paragraph class="contact" :text="phoneNumber" />
   </div>
   <div v-if="email" class="contact-line">
-    <img src="@/assets/email.svg" />
     <a class="nav-link nav-social" style="text-decoration: none;" :href="'mailto:' + email" title="Email">
-      <Paragraph class="contact" :text="email" />
+      <img src="@/assets/email.svg" />
     </a>
+    <Paragraph class="contact" :text="email" />
   </div>
   <div class="contact-line">
     <!--  Keep in mind that the size of SVGs should be 24 * 24  -->
-    <a v-if="linkedIn" class="nav-link nav-social" style="text-decoration: none; margin: 4px;" :href="linkedIn" title="Linked In"><img src="@/assets/linkedin.svg"/></a>
-    <a v-if="telegram" class="nav-link nav-social" style="text-decoration: none; margin: 4px;" :href="telegram" title="Telegram"><img src="@/assets/telegram.svg"/></a>
-    <a v-if="gitlab" class="nav-link nav-social" style="text-decoration: none; margin: 4px;" :href="gitlab" title="Gitlab"><img src="@/assets/gitlab.svg"/></a>
-    <a v-if="github" class="nav-link nav-social" style="text-decoration: none; margin: 4px;" :href="github" title="Github"><img src="@/assets/github.svg"/></a>
-    <a v-if="twitter" class="nav-link nav-social" style="text-decoration: none; margin: 4px;" :href="twitter" title="Twitter"><img src="@/assets/twitter.svg"/></a>
+    <a v-if="linkedIn" class="nav-link nav-social" style="text-decoration: none; margin: 3px 8px 0px 0px;" :href="linkedIn" title="Linked In"><img src="@/assets/linkedin.svg"/></a>
+    <a v-if="telegram" class="nav-link nav-social" style="text-decoration: none; margin: 3px 8px 0px 0px;" :href="telegram" title="Telegram"><img src="@/assets/telegram.svg"/></a>
+    <a v-if="gitlab" class="nav-link nav-social" style="text-decoration: none; margin: 3px 8px 0px 0px;" :href="gitlab" title="Gitlab"><img src="@/assets/gitlab.svg"/></a>
+    <a v-if="github" class="nav-link nav-social" style="text-decoration: none; margin: 3px 8px 0px 0px;" :href="github" title="Github"><img src="@/assets/github.svg"/></a>
+    <a v-if="twitter" class="nav-link nav-social" style="text-decoration: none; margin: 3px 8px 0px 0px;" :href="twitter" title="Twitter"><img src="@/assets/twitter.svg"/></a>
   </div>
 </template>
 
